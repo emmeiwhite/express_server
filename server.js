d0f155403f91bdf3e57c5adf73b18eed809b8407
imran <imran@giveindia.org>
2019-02-22 16:47:48 +0530

    There can be different middlewares in between before the respond is send, the respond may be served from any of the middlewares

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const app = express();
 const html = '<div> Far over the Misty Mountains cold. <h1>We must away, air break of day ... to find our long forgotten gold :)</h1></div>';
 
 // middleware
-app.use(express.static('publicdonkey'));
+app.use(function (req, res, next) {
+    if (req.path.match(/^\/lol$/)) {
+        return res.send(html);
+    }
+    else {
+        return next();
+    }
+});
 
 app.get('/', function(req,res){
     res.send(html);
